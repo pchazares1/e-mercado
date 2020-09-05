d026b6ab64e25de7da3e3323cfc9feb21f70841b
pchazares <pchazares@ggc.edu>
2020-09-05 12:28:42 -0400

    Added signup route, db connection, schema, and some valdiation

---
+++ t-mercado-app/server/models/account.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+// RFC 2822
+
+const matchEmail = validate => {
+    console.log(validate + " " + accountSchema.email.value);
+    if (validate === accountSchema.email) return validate;
+}
+
+const matchPassword = validate => {
+    if(validate === accountSchema.password) return validate;
+}
+
+const accountSchema = mongoose.Schema({
+    fname: {
+        type: String,
+        required: [true, 'First name is required']
+    },
+    lname: {
+        type: String,
+        required: [true, 'Last name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        minlength: [3, 'Email cannot be less than 3 letters'],
+        match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Not a valid email']
+    },
+    cemail: {
+        type: String,
+        required: [true, 'Confirmation password is required'],
+        minlength: 3,
+        validate: [matchEmail, 'Must match your email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        min: [8, 'Password cannot be less than 8 letters']
+    },
+    cpassword: {
+        type: String,
+        required: [true, 'Confirmation password is required'],
+        min: 8,
+        validate: [matchPassword, 'Must match your password']
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Account', accountSchema);
--- t-mercado-app/server/routes/auth/auth.js
+++ t-mercado-app/server/routes/auth/auth.js
@@ -1,8 +1,27 @@
 const express = require('express');
-const router =  express.Router();
+const router = express.Router();
+const Account = require('../../models/account');
 
-router.get('/signup', (req, res) => {
-    res.status(200).json('In the signup');
+router.post('/signup', async (req, res) => {
+    const emailExists = await Account.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).json({ error: 'Email already exists' });
+
+    const account = new Account({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        cemail: req.body.cemail,
+        password: req.body.password,
+        cpassword: req.body.cpassword,
+    });
+    
+    account.save(error => {
+        if(error) {
+        console.log(error);
+        res.status(400).json(error.errors);
+        }
+        res.status(200).json({error: 'Account successfully created'})
+    });
 })
 
 module.exports = router;
