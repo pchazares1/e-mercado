d2f6ac5c0eadf6a338532bef7e9e470a8b4d7ebf
pchazares <pchazares@ggc.edu>
2020-09-07 16:51:08 -0400

    Added errorFormat.js and suitable account json error response

--- t-mercado-app/server/routes/auth/auth.js
+++ t-mercado-app/server/routes/auth/auth.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Account = require('../../models/account');
+const errorFormat = require('./errorFormat');
 
 // return a map of error key and error message value
-const errorFormat = error => {
-    const errorMap = new Map();
-    const errorMsg = error.substring(error.indexOf(':') + 1).trim();
-    const errorArr = errorMsg.split(',').map(err => err.trim());
-    errorArr.forEach(err => {
-        let colonSplit = err.indexOf(':');
-        errorMap.set(err.substring(0, colonSplit), err.substring(colonSplit + 1).trim());
-    });
 
-    return errorMap;
-}
 
 router.post('/signup', async (req, res) => {
     const emailExists = await Account.findOne({ email: req.body.email });
@@ -32,13 +23,7 @@ router.post('/signup', async (req, res) => {
         const savedAccount = await account.save();
         res.status(200).json({ message: 'Account successfully created' });
     } catch (error) {
-        const errorMap = errorFormat(error.message);
-        console.log(errorMap);
-        res.status(400).json({
-            errors: errorMap.forEach((key, value) => {
-                    return key + ' ' + value;
-            })
-        });
+        res.status(400).json({ message: error.message });
     }
 });
 
