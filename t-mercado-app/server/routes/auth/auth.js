1229bcd674df95fc36092689620a23d55dad1eca
pchazares <pchazares@ggc.edu>
2020-09-20 00:19:22 -0400

    Revert "Argon2 hashed passwords"
    This reverts commit 4e4eb89f5577f893e96fff81cd003a71a5f4bd8a.

--- t-mercado-app/server/routes/auth/auth.js
+++ t-mercado-app/server/routes/auth/auth.js
@@ -2,36 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Account = require('../../models/account');
 const errorFormat = require('./errorFormat');
-const argon2 = require('argon2');
 
 // return a map of error key and error message value
 
 
 router.post('/signup', async (req, res) => {
     const emailExists = await Account.findOne({ email: req.body.email });
-    let hashPassword = '';
     if (emailExists) return res.status(400).json({ error: 'Email already exists' });
-    
     // TODO: change method of validating confirmation email and password within the schema
-    if (req.body.cemail !== req.body.email) return res.status(400).json({ error: 'Must match email' });
-    
-    if (req.body.cpassword !== req.body.password) {
-        return res.status(400).json({ error: 'Must match password' });
-    } else {
-        // hash password
-        try {
-            hashPassword = await argon2.hash(req.body.password);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    if (req.body.cemail !== req.body.email) return res.status(400).json({ error: 'Must match email'});
+    if (req.body.cpassword !== req.body.password) return res.status(400).json({ error: 'Must match password'});
 
     const account = new Account({
         fname: req.body.fname,
         lname: req.body.lname,
         email: req.body.email,
         cemail: req.body.cemail,
-        password: hashPassword,
+        password: req.body.password,
         cpassword: req.body.cpassword,
     });
 
