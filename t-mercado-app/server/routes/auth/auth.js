6016e376ca89f746ab4a87902260d19ef6035041
pchazares <pchazares@ggc.edu>
2020-09-20 00:02:03 -0400

    Added validation in login route

--- t-mercado-app/server/routes/auth/auth.js
+++ t-mercado-app/server/routes/auth/auth.js
@@ -2,40 +2,89 @@ const express = require('express');
 const router = express.Router();
 const Account = require('../../models/account');
 const argon2 = require('argon2');
+const { body, validationResults } = require('express-validator');
 
-router.post('/signup', async (req, res) => {
-    const emailExists = await Account.findOne({ email: req.body.email });
-    let hashPassword = '';
-    if (emailExists) return res.status(400).json({ error: 'Email already exists' });
+router.post('/signup', [
+    body('fname')
+        .notEmpty()
+        .bail()
+        .isString()
+        .bail()
+        .isLength({ min: 2 })
+        .bail()
+        .isAlpha()
+        .bail(),
+    body('lname')
+        .notEmpty()
+        .bail()
+        .isString()
+        .bail()
+        .isLength({ min: 2 })
+        .bail()
+        .isAlpha()
+        .bail(),
+    body('email')
+        .notEmpty()
+        .bail()
+        .isLength({ min: 3 })
+        .bail()
+        .isEmail(),
+    body('cemail')
+        .notEmpty()
+        .bail()
+        .custom((value, { req }) => {
+            if (value !== req.body.email) {
+                throw new Error('Must match email');
+            }
+        })
+        .bail(),
+    body('password')
+        .notEmpty()
+        .bail()
+        .isLength({ min: 8 })
+        .bail()
+        .matches((/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,})$/, "i"),
+    body('cpassword')
+        .notEmpty()
+        .bail()
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Must match password');
+            }
+        })],
+    async (req, res) => {
+        const emailExists = await Account.findOne({ email: req.body.email });
+        let hashPassword = '';
+        if (emailExists) return res.status(400).json({ error: 'Email already exists' });
 
-    // TODO: change method of validating confirmation email and password within the schema
-    if (req.body.cemail !== req.body.email) return res.status(400).json({ error: 'Must match email' });
+        // TODO: change method of validating confirmation email and password within the schema
+        if (req.body.cemail !== req.body.email) return res.status(400).json({ error: 'Must match email' });
 
-    if (req.body.cpassword !== req.body.password) {
-        return res.status(400).json({ error: 'Must match password' });
-    } else {
-        // hash password
-        try {
-            hashPassword = await argon2.hash(req.body.password);
-        } catch (error) {
-            console.log(error);
+        if (req.body.cpassword !== req.body.password) {
+            return res.status(400).json({ error: 'Must match password' });
+        } else {
+            // hash password
+            try {
+                hashPassword = await argon2.hash(req.body.password);
+            } catch (error) {
+                console.log(error);
+            }
         }
-    }
 
-    const account = new Account({
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        password: hashPassword
+        const account = new Account({
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.email,
+            password: hashPassword
+        });
+
+        try {
+            const savedAccount = await account.save();
+            res.status(200).json({ message: 'Account successfully created' });
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
     });
-
-    try {
-        const savedAccount = await account.save();
-        res.status(200).json({ message: 'Account successfully created' });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
 
 router.post('/login', async (req, res) => {
     /**
@@ -60,7 +109,7 @@ router.post('/login', async (req, res) => {
         let isPass = true;
         if (!user) {
             res.status(401).json({ error: 'Email does not exist' });
-        } 
+        }
         else if (user) {
             isPass = argon2.verify(user.password, req.body.password);
         }
